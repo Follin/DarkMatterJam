d0d4ffe0ecc997885d3b1f33f9a91e457482ef3a
Haqvin Bager <haqvin.bager@gmail.com>
2019-11-09 14:50:38 +0100

    Test scrip player movement

---
+++ Assets/Test/Haqvin/TestMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+    [SerializeField]
+    private float _speed = 10f;
+
+    void Update() {
+        float movement_y = Input.GetAxis("Vertical") * _speed;
+        float movement_x = Input.GetAxis("Horizontal") * _speed;
+        movement_x *= Time.deltaTime;
+        movement_y *= Time.deltaTime;
+        transform.Translate(movement_x, movement_y, 0);
+    }
+}
