af5fe0d7952f3ddb8c5ac8d696b7a7aa201259b1
Haqvin Bager <haqvin.bager@gmail.com>
2019-11-10 10:08:15 +0100

    Modified player movement and prefab

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 //TODO: Collision detection!
 public class PlayerMovement : MonoBehaviour {
+    Rigidbody _rb;
     [SerializeField]
     private float _speed = 2f;
 
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     void Update() {
         float movement_y = Input.GetAxis("Vertical") * _speed;
         float movement_x = Input.GetAxis("Horizontal") * _speed;
 
-        transform.position = transform.position + new Vector3(movement_x * _speed * Time.deltaTime, movement_y * _speed * Time.deltaTime, 0);
+        _rb.transform.position += new Vector3(movement_x * _speed * Time.deltaTime, movement_y * _speed * Time.deltaTime, 0);
     }
 }
