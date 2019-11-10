2bb6d8a381df61aadf39fae34dc87ec4b1a7267a
Ellen <ellen.subraian@gmail.com>
2019-11-10 11:36:02 +0100

    editerat menyknappar med typsnitt

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -17,6 +17,8 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] Transform _player;
     
 
+    public GameObject EnemyPrefab;
+
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
