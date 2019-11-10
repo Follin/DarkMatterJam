248cb0041caa074d3a9a9b390c7abceed56f3f87
Ellen <ellen.subraian@gmail.com>
2019-11-10 13:55:57 +0100

    MENYER

--- Assets/Scripts/MenuSystem/MainMenu.cs
+++ Assets/Scripts/MenuSystem/MainMenu.cs
@@ -8,12 +8,27 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void Quit()
     {
         Application.Quit();   
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
     }
 
     private void Start()
